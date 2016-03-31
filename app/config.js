a1021d04383b69edbeea043c8a1962c052166faa
Hack Reactor Students <accounts@hackreactor.com>
2016-03-31 16:46:47 -0700

    replace sqlite with mongo

--- app/config.js
+++ app/config.js
@@ -1,31 +1,11 @@
-var path = require('path');
-var db = require('mongoose');
+var mongoose = require('mongoose');
 
-var urlSchema = new db.schema({
-      id: {type: Number, required: true, unique: true },
-      link.string('url', 255);
-      link.string('baseUrl', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
+mongoose.connect('mongodb://localhost/shortlydb');
 
-var userSchema = new db.schema(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongodb connection open');
 });
 
 module.exports = db;
